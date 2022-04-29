d0c8d9865564037386402a07a1c9d291ffa65092
SeungGu Kang <alex9366@naver.com>
2022-04-30 05:25:40 +0900

    변수(var)와 상수(val)

--- study02.kt
+++ study02.kt
@@ -2,3 +2,15 @@ fun main(){
     print("hello world!")
 }
 
+//val : 상수 --> 값 변경 불가
+//var : 변수 --> 값 변경 가능
+fun hi(){
+    val a : Int = 10
+    var b : Int = 9
+
+    //변수를 선언할 때에는 변수 타입을 선언하지 않아도 된다.
+    val c = 100
+    var d = 100
+
+    var name = "Kang"
+}
