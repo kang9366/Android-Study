5b6f167455b1dfb8810a88ed8f9d0529a07e7ea5
SeungGu Kang <alex9366@naver.com>
2022-04-30 05:32:13 +0900

    String template

---
+++ study03.kt
@@ -0,0 +1,10 @@
+//String template
+
+fun main(){
+    val name = "Kang"
+    println("My name is $name")
+    //string template 다음 한 칸은 반드시 띄워쓰기를 해야한다.
+    println("My name is $name I'm 25.")
+    //띄어쓰기를 하고 싶지 않다면 {}사용
+    println("My name is ${name}I'm 25.")
+}
