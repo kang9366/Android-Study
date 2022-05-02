4e742b97dea29707ba653975fe300ff3749e6f61
SeungGu Kang <alex9366@naver.com>
2022-05-03 03:23:26 +0900

    클래스 활용

---
+++ study09.kt
@@ -0,0 +1,23 @@
+fun main(){
+    val calculator1 = Calculator1()
+    calculator1.plus(1, 2)
+}
+
+class Calculator1{
+    fun plus(num1: Int, num2: Int):Int{
+        return (num1 + num2)
+    }
+    fun minus(num1: Int, num2: Int):Int{
+        return (num1 - num2)
+    }
+    fun divide(num1: Int, num2: Int):Int{
+        return (num1 * num2)
+    }
+    fun multiply(num1: Int, num2: Int):Int{
+        return (num1 / num2)
+    }
+}
+
+class Calculator2(){
+    fun plus
+}
