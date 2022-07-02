ebbe863a71670d8c6079b44be1f03232dab84bd5
Kang SeungGu <63611804+kang9366@users.noreply.github.com>
2022-07-02 11:06:37 +0900

    Lambda

--- Kotlin_study15.kt
+++ Kotlin_study15.kt
@@ -1,44 +1,48 @@
-//lambda
-//람다식은 value처럼 다룰 수 있는 익명함수이다.
-//1. 함수를 메소드의 파라미터로 넘겨줄 수 있다.
-//2. 함수를 함수의 return값으로 사용할 수 있다.
+//순수함수
+fun sum(x:Int, y:Int): Int{
+    return x+y
+}
 
+//람다
 //람다의 기본 형식
 //val lambdaName : Type = {argmentList -> codeBody}
 
-val square : (Int) -> (Int) = {number -> number*number}
-//input과 return값의 타입은 생략 가능
+val lambdaSum: (Int, Int) -> Int = {x, y ->
+    x+y
+}
+
 val nameAge = {name: String, age: Int -> "My name is $name and I'm $age"}
 
-//람다의 return
-val calculateGrade: (Int) -> String = {
-    when(it){
-        in 0..40 -> "fail"
-        in 41..70 -> "pass"
-        in 71..100 -> "perfect"
-        else -> "Error"
-    }
+//람다의 마지막 줄은 반환값이다.
+val lambdaSum2: (Int, Int) -> Int = {x, y ->
+    val sum = x+y
+    sum
 }
 
-//람다를 표현하는 여러가지 방법
-fun invokeLambda(lambda: (Double) -> Boolean): Boolean{
-    return lambda(5.37)
+//매개변수가 없는 람다함수 --> 괄호를 꼭 써주어야 한다.
+val lam: () -> Unit = {
+    println("Hello world!")
 }
 
-//확장함수
-val pizzaIsGreat : String.() -> String = {
-    this + "Pizza is the best!"
+//매개변수가 하나인 경우 "매개변수명 -> " 부분은 생략이 가능하고 함수 내부에서 사용할때에는 it으로 사용한다.
+//단 매개변수가 2개 이상일때에는 생략이 불가능 하다.
+val lam2: (String) -> Unit = {
+    println(it)
 }
+
+//미사용 매개변수가 존재하는 경우에는 '_'으로 변수명 지칭을 생략 가능하다.
+val lam3: (Int, Int, Int) -> Int = {x, y, _ ->
+    x+y
+}
+
+fun Test(a:(Int, Int) -> Int, b:Int, c: Int) = println(a(b, c))
+
 
 fun main(){
-    //람다
-    println(square(10))
-    println(nameAge("kang", 25))
-    println(calculateGrade(97))
-
-    //확장함수
-    val a = "kim said "
-    val b = "lee said "
-    println(a.pizzaIsGreat())
-    println(b.pizzaIsGreat())
+    Test(lambdaSum, 2, 3)
+    //순수함수를 다른 함수의 매개변수로 호출할때에는 이름으로 호출이 불가능하다. '::'을 함수 이름앞에 붙혀서 호출
+    Test(::sum, 2, 3)
+    lam()
+    lam2("Hello world!")
+    println(lam3(1, 2, 3))
 }
